af9e73c22c7104703583c543356a50ba8d33a31c
Rajnesh Kumar <109650270+rajyadav0001@users.noreply.github.com>
2023-06-10 16:42:30 +0530

    Adding light & dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import {Routes, Route} from 'react-router-dom';
 import Navbar from "./components/Home-Page/Navbar/Navbar";
-// import TopImgSlider from "./components/Home-Page/Hero-section/ImgSlider/TopImgSlider";
-// import {slides} from "./components/Home-Page/Hero-section/ImgSlider/SliderData.json"
-// import Footer from "./components/Home-Page/Footer/Footer";
 import Contact from "./components/Contact";
 import Home from "./components/Home-Page/Home";
 const App = () => {
--- src/components/Home-Page/Navbar/Navbar.jsx
+++ src/components/Home-Page/Navbar/Navbar.jsx
@@ -3,13 +3,36 @@ import { Link } from "react-router-dom";
 import Logo from "../../../assets/Logo.jpeg";
 import Button from "./Button";
 import NavLinks from "./NavLinks";
+import {AiOutlineMail} from 'react-icons/ai';
+import {BsTelephone} from 'react-icons/bs';
+import {BsFillMoonFill} from 'react-icons/bs';
+import {BsFillSunFill} from 'react-icons/bs';
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const [themIcon,setThemIcon] = useState(true); 
   const OnSlideChange = () =>{
        setOpen(false);
   }
+  const changeThemState = () =>{
+    if(themIcon=== true){
+      setThemIcon(false);
+    }else{
+      setThemIcon(true);
+    }
+  }
   return (
     <nav className="bg-white">
+    <div className=" bg-black p-2 text-white flex justify-end md:pr-10 pr-6 ">
+        <div className="flex md:gap-7 gap-2 md:ml-0 ml-3 flex-wrap pr-16 ">
+           <p className="flex items-center gap-2"><AiOutlineMail/>dummy@gmail.com</p>
+           <p className="flex items-center gap-2"><BsTelephone/>Phone No</p>
+        </div>
+        <div className="flex items-center" onClick={changeThemState}>
+        {
+          themIcon ?  <BsFillMoonFill className="text-white md:text-2xl text-3xl"/> : <BsFillSunFill className="text-white md:text-2xl text-3xl"/>
+        }    
+        </div>
+    </div>
       <div className="flex items-center font-medium justify-around">
         <div className="z-50 p-5 md:w-auto w-full flex justify-between">
          <Link to='/'><img onClick={OnSlideChange} src={Logo} alt="logo" className="md:cursor-pointer h-12 rounded-md" /></Link>
@@ -41,7 +64,7 @@ const Navbar = () => {
         {/* Mobile nav */}
         <ul
           className={` z-20
-        md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-24 pl-4
+        md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-36 pl-4
         duration-500 ${open ? "left-0" : "left-[-100%]"}
         `}
         >
