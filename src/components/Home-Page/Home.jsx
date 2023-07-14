586f396c19fc51f88ce5f2b4835fdffd2b13c84d
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-07-14 13:49:05 +0530

    prop removed

--- src/components/Home-Page/Home.jsx
+++ src/components/Home-Page/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {slides} from '../Home-Page/Hero-section/ImgSlider/SliderData.json'
 import TopImgSlider from './Hero-section/ImgSlider/TopImgSlider'
 import Footer from './Footer/Footer';
 import Industries from '../IndustriesWeServe/Industries';
@@ -9,7 +8,7 @@ import '../../index.css';
 const Home = () => {
   return (
     <div className='bgClr'>
-      <TopImgSlider data={slides} />
+      <TopImgSlider />
       <Industries/>
       <Footer/>
     </div>
