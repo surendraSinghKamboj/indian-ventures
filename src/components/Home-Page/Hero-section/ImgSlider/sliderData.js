a779e5d499ea37d17517e4232060beb1a16c3212
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-07-14 13:41:08 +0530

    slider updated

---
+++ src/components/Home-Page/Hero-section/ImgSlider/sliderData.js
@@ -0,0 +1,19 @@
+import img_one from "../../../assets/img1.png";
+import img_two from "../../../assets/img2.png";
+import img_three from "../../../assets/img3.png";
+
+
+export const slides = [
+    {
+        src:img_one,
+        alt:"slide on"
+    },
+    {
+        src:img_two,
+        alt:"slides second"
+    },
+    {
+        src:img_three,
+        alt:"slide third"
+    }
+]
