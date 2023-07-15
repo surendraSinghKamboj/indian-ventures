c3f9ccac90e8433bac93d6e5cfe7a31d977aaed4
rachna rajak <113829584+rachnarajaka040@users.noreply.github.com>
2023-07-15 12:44:40 +0530

    add

--- src/components/Home-Page/Hero-section/ImgSlider/sliderData.js
+++ src/components/Home-Page/Hero-section/ImgSlider/sliderData.js
@@ -1,6 +1,10 @@
-import img_one from "../../../assets/img1.png";
-import img_two from "../../../assets/img2.png";
-import img_three from "../../../assets/img3.png";
+import img_one from "../../../../assets/img1.png";
+import img_two from "../../../../assets/img2.png";
+import img_three from "../../../../assets/img3.png";
+import img_four from "../../../../assets/img4.png";
+import img_five from "../../../../assets/img5.png";
+import img_six from "../../../../assets/img6.png";
+import img_seven from "../../../../assets/img7.png";
 
 
 export const slides = [
@@ -14,6 +18,22 @@ export const slides = [
     },
     {
         src:img_three,
+        alt:"slide third"
+    },
+    {
+        src:img_four,
+        alt:"slide third"
+    },
+    {
+        src:img_five,
+        alt:"slide third"
+    },
+    {
+        src:img_six,
+        alt:"slide third"
+    },
+    {
+        src:img_seven,
         alt:"slide third"
     }
 ]
