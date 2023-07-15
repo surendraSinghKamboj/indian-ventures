9c2261c73c31372cde10b25dcaf50b34191556e3
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-07-15 13:17:05 +0530

    Update Pyramid.jsx

--- src/components/Pyramid.jsx
+++ src/components/Pyramid.jsx
@@ -2,51 +2,45 @@ import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 
 const Pyramid = () => {
-      const containerRef = useRef();
+  const containerRef = useRef();
 
-        useEffect(() => {
-                let frameId;
-                    let pyramid;
+  useEffect(() => {
+    let frameId;
+    let pyramid;
 
-                        const scene = new THREE.Scene();
-                            const camera = new THREE.PerspectiveCamera(
-                                      75,
-                                            containerRef.current.clientWidth / containerRef.current.clientHeight,
-                                                  0.1,
-                                                        1000
-                            );
-                                const renderer = new THREE.WebGLRenderer({ antialias: true });
-                                    renderer.setSize(containerRef.current.clientWidth, containerRef.current.clientHeight);
-                                        containerRef.current.appendChild(renderer.domElement);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      containerRef.current.clientWidth / containerRef.current.clientHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(containerRef.current.clientWidth, containerRef.current.clientHeight);
+    containerRef.current.appendChild(renderer.domElement);
 
-                                            const geometry = new THREE.ConeGeometry(1, 2, 4);
-                                                const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-                                                    pyramid = new THREE.Mesh(geometry, material);
-                                                        scene.add(pyramid);
+    const geometry = new THREE.ConeGeometry(1, 2, 4);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    pyramid = new THREE.Mesh(geometry, material);
+    scene.add(pyramid);
 
-                                                            camera.position.z = 5;
+    camera.position.z = 5;
 
-                                                                const animate = () => {
-                                                                          frameId = requestAnimationFrame(animate);
-                                                                                pyramid.rotation.x += 0.01;
-                                                                                      pyramid.rotation.y += 0.01;
-                                                                                            renderer.render(scene, camera);
-                                                                };
-                                                                    animate();
+    const animate = () => {
+      frameId = requestAnimationFrame(animate);
+      pyramid.rotation.x += 0.01;
+      pyramid.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
 
-                                                                        return () => {
-                                                                                  cancelAnimationFrame(frameId);
-                                                                                        containerRef.current.removeChild(renderer.domElement);
-                                                                        };
-                                                                    }, []);
+    return () => {
+      cancelAnimationFrame(frameId);
+      containerRef.current.removeChild(renderer.domElement);
+    };
+  }, []);
 
-                                                                      return <div ref={containerRef} />;
-                                                                };
+  return <div ref={containerRef} />;
+};
 
-                                                                export default Pyramid;
-                                                                
-                                                                        }
-                                                                }
-                            )
-        })
-}
+export default Pyramid;
