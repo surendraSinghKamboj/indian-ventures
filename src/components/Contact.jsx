38dc4891db3762c6f1946a8326d5c813a449d066
rachna rajak <113829584+rachnarajaka040@users.noreply.github.com>
2023-07-26 21:24:19 +0530

    add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,71 +16,62 @@ const Contact = () => {
     }
     
   return (
-    <div className="bgClr">
-     <hr/>
-      <section className="text-gray-600 body-font relative">
-        <div className="container px-5 py-16 mx-auto">
-          <div className="flex flex-col text-center w-full mb-12">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 headClr">
-              Contact Us
-            </h1>
-             
-          </div>
-          <div className="lg:w-1/2 md:w-2/3 mx-auto">
-            <div className="flex flex-wrap -m-2">
-              <div className="p-2 w-1/2">
-                <div className="relative">
-                  <label htmlFor="name" className="leading-7 text-sm text-gray-600 headClr">
-                    Name
-                  </label>
-                  <input
-                    onChange={inputChange}
-                    type="text"
-                    id="name"
-                    name="name"
-                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
-                </div>
-              </div>
-              <div className="p-2 w-1/2">
-                <div className="relative">
-                  <label htmlFor="email" className="leading-7 text-sm text-gray-600 headClr">
-                    Email
-                  </label>
-                  <input
-                    onChange={inputChange}
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
-                </div>
-              </div>
-              <div className="p-2 w-full">
-                <div className="relative">
-                  <label htmlFor="message" className="leading-7 text-sm text-gray-600 headClr">
-                    Message
-                  </label>
-                  <textarea
-                    onChange={inputChange}
-                    id="message"
-                    name="message"
-                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                  ></textarea>
-                </div>
-              </div>
-              <div className="p-2 w-full">
-                <button onClick={HandleData} className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                  Submit
-                </button>
-              </div>
-               
-            </div>
-          </div>
+    <div class="form-main">
+    <div class="main-wrapper">
+      <h2 class="form-head">Contact From</h2>
+      <form class="form-wrapper">
+        <div class="form-card">
+          <input
+            class="form-input"
+            type="text"
+            name="full_name"
+            required="required"
+          />
+          <label class="form-label" for="full_name">Full Name</label>
         </div>
-      </section>
+
+        <div class="form-card">
+          <input
+            class="form-input"
+            type="email"
+            name="email"
+            required="required"
+          />
+          <label class="form-label" for="email">Email</label>
+        </div>
+
+        <div class="form-card">
+          <input
+            class="form-input"
+            type="number"
+            name="phone_number"
+            required="required"
+          />
+          <label class="form-label" for="phone_number">Phone number</label>
+        </div>
+
+        <div class="form-card">
+          <textarea
+            class="form-textarea"
+            maxlength="420"
+            rows="3"
+            name="phone_number"
+            required="required"
+          ></textarea>
+          <label class="form-textarea-label" for="phone_number"
+            >Address</label
+          >
+        </div>
+        <div class="btn-wrap">
+          <button> Submit </button>
+        </div>
+      </form>
     </div>
+  </div>
   );
 };
 
 export default Contact;
+
+
+
